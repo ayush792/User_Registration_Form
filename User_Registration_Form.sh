ff2e6a1b413eb8ef9590e5ab15f1884ccc13e08c
Ayush <vayush198@gmail.com>
2020-04-30 17:00:30 +0530

    Refactor:operation are converted into functions

--- User_Registration_Form.sh
+++ User_Registration_Form.sh
@@ -2,22 +2,31 @@
 
 shopt -s extglob 		#Extended global
 
+function  firstName(){
 echo "Enter the user First Name"
 read user
 first_name_pattern='^[A-Z]{1}[a-z]{2,}$'
+
 if [[ $user =~ $first_name_pattern ]]
 then
 	echo valid
 else
 	echo invalid
 fi
+}
 
+function lastName(){
 echo "Enter the last name"
 read user
 last_name_pattern='[A-Z]{1}[a-z]{2,}$'
+
 if [[ $user =~ $last_name_pattern ]]
 then
 	echo valid
 else
 	echo invalid
 fi
+}
+
+firstName
+lastName
