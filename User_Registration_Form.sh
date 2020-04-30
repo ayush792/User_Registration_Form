e2f839076536c12b3215cc206b7a70fb3f5c79d0
Ayush <vayush198@gmail.com>
2020-04-30 16:36:29 +0530

    Add:Added UC-2, last name starts with cap and min 3 char

--- User_Registration_Form.sh
+++ User_Registration_Form.sh
@@ -5,8 +5,17 @@ shopt -s extglob 		#Extended global
 echo "Enter the user First Name"
 read user
 first_name_pattern='^[A-Z]{1}[a-z]{2,}$'
-
 if [[ $user =~ $first_name_pattern ]]
+then
+	echo valid
+else
+	echo invalid
+fi
+
+echo "Enter the last name"
+read user
+last_name_pattern='[A-Z]{1}[a-z]{2,}$'
+if [[ $user =~ $last_name_pattern ]]
 then
 	echo valid
 else
