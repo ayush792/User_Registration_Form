ea1c1280088fe9d250c07a319863ea797ef42d21
Ayush <vayush198@gmail.com>
2020-05-03 18:19:24 +0530

    Add:Added UC-5,6,7,8, password function to validate password

--- User_Registration_Form.sh
+++ User_Registration_Form.sh
@@ -2,60 +2,68 @@
 
 shopt -s extglob 		#Extended global
 
-function  firstName(){
-echo "Enter the user First Name"
-read user
 first_name_pattern='^[A-Z]{1}[a-z]{2,}$'
+last_name_pattern='^[A-Z]{1}[a-z]{2,}$'
+email_pattern='^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$'
+mobile_number_pattern="([0-9]{2}[[:space:]])?[0-9]{10}$"
+password_pattern="^[A-Za-z0-9]*[@#$%^&*][0-9a-zA-Z]*$"
 
-if [[ $user =~ $first_name_pattern ]]
-then
-	echo valid
-else
-	echo invalid
-fi
+function patternValidation(){
+
+	if [[ $1 =~ $2 ]]
+	then
+		echo Valid
+	else
+		echo invalid
+	fi
+}
+
+function  firstName(){
+
+	echo "Enter the user First Name"
+	read user
+	patternValidation $user $first_name_pattern 
+
 }
 
 function lastName(){
-echo "Enter the last name"
-read user
-last_name_pattern='^[A-Z]{1}[a-z]{2,}$'
 
-if [[ $user =~ $last_name_pattern ]]
-then
-	echo valid
-else
-	echo invalid
-fi
+	echo "Enter the last name"
+	read user
+	patternValidation $user $last_name_pattern 
 }
 
 function email(){
-echo "Enter your email address"
-read email
-email_pattern="[a-zA-Z0-9]+([._-+][a-zA-Z0-9]+)*@[a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 
-if [[ $email =~ $email_pattern ]]
-then
-	echo valid
-else
-	echo invalid
-fi
-
+	echo "Enter your email address"
+	read email
+	patternValidation $email $email_pattern 
 }
 
 function mobileNumber(){
-echo "Enter your mobile number with country code"
-read mobileNumber
-mobile_number_pattern='^([0-9]{1,4}[[:space:]])?[0-9]{10}$'
 
-if [[ $mobileNumber =~ $mobile_number_pattern ]]
-then
-	echo valid
-else
-	echo invalid
-fi
+	echo "Enter your mobile number with country code"
+	read mobile_number
+	patternValidation $mobile_number $mobile_number_pattern 
+}
+
+function checkPassword(){
+
+	echo "Please Enter Password"
+	read password
+	count =`echo ${#password}`
+
+	if [[ $count -ne 8 ]]
+	then
+		echo "Password should have atleast 8 character"
+	exit 1;
+	fi
+
+	patternValidation $password $password_pattern
 }
 
 firstName
 lastName
 email
 mobileNumber
+checkPassword
